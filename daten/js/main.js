cfc94e6588eb8042d34cf5ab87505fb944d70517
Felix Schett <setuini@Felixs-MacBook-Pro-2.local>
2018-11-28 17:31:12 +0100

    Fix Modal beeing visible before onclick Event

--- daten/js/main.js
+++ daten/js/main.js
@@ -12,8 +12,8 @@ D 0049 152 294 963 44
 **/
 
 jQuery(window).on('load', function() {
-      
-    
+
+
     // HIDE PRELAODER
     $(".preloader").addClass("preloader-hidden");
 
@@ -34,39 +34,39 @@ jQuery(window).on('load', function() {
 
     }, 900 );
 
-    
+
 });
 
 
 jQuery(document).ready(function($) {
 	"use strict";
-    
-    
+
+
     $(window).on('load', function() {
-        
+
         // HIDE PRELAODER
         $(".preloader").addClass("preloader-hidden");
-        
+
         // SHOW/ANIMATE ANIMATION CONTAINER
         setTimeout(function(){
-            
+
             $(".hero .animation-container").each(function(){
 
                 var e = $(this);
 
                 setTimeout(function(){
-                    
+
                     e.addClass("run-animation");
-                    
+
                 }, e.data("animation-delay") );
 
             });
-            
+
         }, 900 );
-        
+
     });
-    
-    
+
+
     // INIT PARALLAX PLUGIN
     $(".hero .background-content.parallax-on").parallax({
         scalarX: 24,
@@ -74,58 +74,56 @@ jQuery(document).ready(function($) {
         frictionX: 0.1,
         frictionY: 0.1,
     });
-    
-    
-    // OPEN POPUP SEQUENCE
+
+
+    //OPEN POPUP SEQUENCE
     $(".open-popup").click(function(){
-        
         $(".popup").addClass("show");
         $(".popup").append('<div class="close-popup backface"></div>');
-        
     });
 
     // CLOSE POPUP SEQUENCE
-    $(document).on('click', '.close-popup', function(){ 
-        
+    $(document).on('click', '.close-popup', function(){
+
         $(".popup").removeClass("show");
         $(".popup .backface").remove();
-        
+
     });
-    
-    
+
+
     // AJAX SUBSCRIBE FORM
-    $('.subscribe-form').submit(function() {
+    // $('.subscribe-form').submit(function() {
+    //
+    //     var postdata = $('.subscribe-form').serialize();
+    //
+    //     $.ajax({
+    //
+    //         type: 'POST',
+    //         url: 'daten/php/subscribe.php',
+    //         data: postdata,
+    //         dataType: 'json',
+    //         success: function(json) {
+    //
+    //             $('.subscribe-form').removeClass("form-error");
+    //
+    //             if(json.valid === 0) {
+    //
+    //                 $('.subscribe-form').addClass("form-error");
+    //
+    //             } else {
+    //
+    //                 $('.subscribe-form').addClass("form-success");
+    //                 $('.subscribe-form input,.subscribe-form button').val('').prop('disabled', true);
+    //
+    //             }
+    //
+    //         }
+    //
+    //     });
+    //
+    //     return false;
+    //
+    // });
 
-        var postdata = $('.subscribe-form').serialize();
 
-        $.ajax({
-
-            type: 'POST',
-            url: 'daten/php/subscribe.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-
-                $('.subscribe-form').removeClass("form-error");
-
-                if(json.valid === 0) {
-                    
-                    $('.subscribe-form').addClass("form-error");
-                    
-                } else {
-
-                    $('.subscribe-form').addClass("form-success");
-                    $('.subscribe-form input,.subscribe-form button').val('').prop('disabled', true);
-                    
-                }
-                
-            }
-
-        });
-
-        return false;
-
-    });
-    
-    
 });
